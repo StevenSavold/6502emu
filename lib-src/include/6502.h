c25abbc889b99ccf3e9d013e4cac084655598e2d
TryOSdev-Steven <steven35953@gmail.com>
2018-04-03 11:37:36 -0700

    Added 6502.h

---
+++ lib-src/include/6502.h
@@ -0,0 +1,18 @@
+/* 6502.h
+ * Authors: Jesse Lembo, Lukas Mallory, Steven Savold
+ * 
+ * This is the main header file for the 6502 emulator library.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+#include <stdint.h>
+
+typedef int8_t  sbyte;
+typedef uint8_t ubyte;
+
+typedef int16_t  sword;
+typedef uint16_t uword;
--- lib-src/include/mylib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MYLIB_H_
-#define _MYLIB_H_
-
-extern int addTwo(int x);
-
-#endif
