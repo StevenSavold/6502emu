c8b1756365099a5d20b9d905b52f3b69c34b2a20
lukeylad19 <37940137+lukeylad19@users.noreply.github.com>
2018-04-10 10:48:10 -0400

    Update FRlib.h

--- lib-src/include/FRlib.h
+++ lib-src/include/FRlib.h
@@ -1,9 +1,7 @@
 /*
 *  PROGRAM NAME: 6502 Emulator Library Header File
 *  PROGRAM DESCRIPTION: This program is used to run an emulator for the-
-**                      6502 CPU. This is the library file header used to store
-**                      function prototypes. The emulator control and library are-
-**                      stored inside the 6502EC.c file and 6502lib.c respectivly;
+**                      6502 CPU. This is the file read header file. 
 *
 * PROGRAM DEVELOPERS: Jesse Lembo + Lukas Mallory;
 * DATE STARTED: MARCH 26, 2018;
