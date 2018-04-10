39e7d40de78a1941004ca2ab44068d7bfaa9161b
lukeylad19 <37940137+lukeylad19@users.noreply.github.com>
2018-04-10 10:24:43 -0400

    Update FR.c

--- src/FR.c
+++ src/FR.c
@@ -5,7 +5,7 @@
 **                      emulator. The emulator itself is contained within-
 **                      the programmer made 6502lib.c and 6502lib.h files.;
 *
-* PROGRAM DEVELOPERS: Jesse Lembo + Lukas Mallory;
+* PROGRAM DEVELOPERS: Jesse Lembo + Lukas Mallory + Steven;
 * DATE STARTED: MARCH 26, 2018;
 * DATE COMPLETED: N/A
  */
@@ -13,11 +13,12 @@
 
 
 #include <stdlib.h>
-#include <FRlib.h>
+#include "6502lib.h"
+#include "FRlib.h"
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <string.h>
 
 int main(int argc , char **argv) {
 //  VARIABLES
@@ -28,36 +29,33 @@ int main(int argc , char **argv) {
     if (argc != 2){
         return 1;
     }
-    //This block is sending information to the FRlib.c file to determine the current file's eligibility for reading.
+    //This block is sending information to the 6502lib.c file to determine the current file's eligibility for reading.
     //The stat function simply gathers the information needed without reading the file in
     struct stat fileStat;
 
     if (stat(argv[1],&fileStat) < 0){
         return 1;
     }
-    //This block is sending information to the FRlib.c file to determine the current file's eligibility for reading
+    //This block is sending information to the 6502lib.c file to determine the current file's eligibility for reading
     printf("\n\n");
     printf("Information for %s\n",argv[1]);
-
     read_okay = test_file(fileStat);
 
-    printf("\n\n%d\n\n" , read_okay);
 
     //If the read_okay variable is 1 the file is cleared to be read in
     if (read_okay == 1){
-
         //Creates a file_ptr called infile to store the file's contents
         FILE *infile;
         infile = fopen(argv[1], "rb");
-
-        //readFile(infile); //readFile currently doesnt exist?
-
+        read_file(infile);
     }
-
     else {
 
         return EXIT_FAILURE;
 
     }
+  //Beginning of the handle_file block; Reads the file line by line to determine instrution unsets
+
+
 
 }
